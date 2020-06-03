a9139b45310a8323e71985eab14f072370b970b2
John Cook <johngncook@gmail.com>
2020-06-03 06:05:23 -0700

    Initial commit.

---
+++ space_invaders.py
@@ -0,0 +1,26 @@
+import pygame
+
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+    # Initialize pygame, settings, and screen object
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    ship = Ship(ai_settings,screen)
+
+    # Start the main loop for the game.
+    while True:
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
+
+
+run_game()
+
