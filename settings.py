c8ba4defb91c4678ff1630fdc8cdf3772ccc1cc2
John Cook <johngncook@gmail.com>
2020-06-03 07:37:15 -0700

    Added bullet class to ship to fire with space bar.

--- settings.py
+++ settings.py
@@ -7,5 +7,12 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
-        #Ship settings
+        # Ship settings
         self.ship_speed_factor = 1.5
+        # Bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+
+
--- space_invaders.py
+++ space_invaders.py
@@ -1,9 +1,11 @@
 import pygame
+from pygame.sprite import Group
 
 
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+
 
 
 def run_game():
@@ -13,13 +15,17 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    # Make a ship.
     ship = Ship(ai_settings,screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
