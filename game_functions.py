63bf9ff6d575abac1354747f068f263bee66a959
John Cook <johngncook@gmail.com>
2020-06-05 19:51:40 -0700

    Added create fleet method, get number of rows, get number of aliens and refactored alien creating functions in the gf module.

--- game_functions.py
+++ game_functions.py
@@ -3,6 +3,48 @@ import sys
 import pygame
 
 from bullet import Bullet
+from alien import Alien
+
+
+def get_number_aliens_x(ai_settings, alien_width):
+    """Determine the number of aliens that fit in a row."""
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x
+
+
+def get_number_rows(ai_settings, ship_height, alien_height):
+    """Determine the number of rows of aliens that fit on the screen."""
+    available_space_y = (ai_settings.screen_height -
+                         (3 * alien_height) - ship_height)
+    number_rows = int(available_space_y / (2 * alien_height))
+    return number_rows
+
+
+def create_alien(ai_settings, screen, aliens, alien_number, row_number):
+    """Create an alien and place it in the row."""
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+    aliens.add(alien)
+
+
+def create_fleet(ai_settings, screen, ship, aliens):
+    """Create a full fleet of aliens."""
+    # Create an alien and find the number of aliens in a row.
+    # Spacing between each alien is equal to one alien width.
+    alien = Alien(ai_settings, screen)
+    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+    number_rows = get_number_rows(ai_settings, ship.rect.height,
+        alien.rect.height)
+
+    # Create the fleet of aliens
+    for row_number in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            create_alien(ai_settings, screen, aliens, alien_number,
+                row_number)
 
 
 def update_bullets(bullets):
@@ -59,14 +101,14 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship, alien,  bullets):
+def update_screen(ai_settings, screen, ship, aliens,  bullets):
     """Update images on the screen and flip to the new screen."""
     screen.fill(ai_settings.bg_color)
     # Redraw all bullets behind ships and aliens.
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
-    alien.blitme()
+    aliens.draw(screen)
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
--- space_invaders.py
+++ space_invaders.py
@@ -4,7 +4,6 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
-from alien import Alien
 import game_functions as gf
 
 
@@ -16,20 +15,20 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    # Make a ship.
-    ship = Ship(ai_settings,screen)
-    # Make a group to store bullets in.
+    # Make a ship, a group of bullets, and a group of aliens.
+    ship = Ship(ai_settings, screen)
     bullets = Group()
+    aliens = Group()
 
-    # Make an alien.
-    alien = Alien(ai_settings, screen)
+    # Create a fleet of aliens
+    gf.create_fleet(ai_settings, screen, ship,  aliens)
 
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien,  bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens,  bullets)
 
 
 run_game()
